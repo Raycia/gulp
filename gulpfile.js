1b919ad9d78754e7729303b54639a6e40b2fedfa
Raycia <raycialima@gmail.com>
2023-06-05 18:42:17 -0300

    aplicando alterações

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,23 @@ function tarefasJS(){
 }
 
 
+function tarefasImagem(){
+    
+    return gulp.src('./src/images/*')
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true
+        }))
+        .pipe(gulp.dest('./dist/images'))
+}
+
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
 exports.images = tarefasImagem
